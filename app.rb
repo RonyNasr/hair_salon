8a1f1b9fb3f4f80b06e17550ef6f71fcfcb4c88f
Rony Nasr <nasr.rony@gmail.com>
2016-07-15 12:03:36 -0700

    added front end for Stylist with integration tests

--- app.rb
+++ app.rb
@@ -11,14 +11,32 @@ get('/') do
   erb(:index)
 end
 
-
 get("/stylists")do
   @stylists = Stylist.all()
+  erb(:stylists)
+end
 
+get("/clients") do
+  erb(:clients)
+end
+
+get("/stylists/new") do
+  erb(:stylists_form)
+end
+
+
+post("/stylists")do
+  stylist = Stylist.new({:name => params.fetch("name")})
+  stylist.save()
+  @stylists = Stylist.all()
   erb(:stylists)
 end
 
 
-get("/clients") do
-  erb(:clients)
+get("/stylists/:id")do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  @clients = @stylist.clients()
+  @stylists = Stylist.all()
+  
+  erb(:stylists)
 end
--- spec/stylist_integration_spec.rb
+++ spec/stylist_integration_spec.rb
@@ -5,12 +5,23 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 
-describe('view stylists page', {:type => :feature}) do
+describe('view stylists page with no stylists', {:type => :feature}) do
   it('visits home page and ges to view stylists page') do
     visit('/')
     click_link('View Stylists')
 
-    expect(page).to have_content('Stylists')
     expect(page).to have_content('You have no stylists yet')
+  end
+end
+
+
+describe('view stylists page with no stylists', {:type => :feature}) do
+  it('visits home page and ges to view stylists page') do
+    visit('/stylists')
+    click_link('Add a Stylist')
+    expect(page).to have_content("Add a Stylist")
+    fill_in("name", :with => "Gary")
+    click_button("Add Stylist")
+    expect(page).to have_content('Gary')
   end
 end
